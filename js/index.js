7d67e09c4fbe9fafeb0883b9698d353ef0eb04ac
Charles Javelona <charlesjavelona@gmail.com>
2016-08-14 22:59:53 -0400

    Query current weather and weatherforecast showing

--- js/index.js
+++ js/index.js
@@ -1,29 +1,40 @@
 //Intitalize global data objects, which will be used to store api objects
-userLocation = {};
+userLocation = {};	//Returns only the that is needed to diplay to the html template
 weatherInfo = {};
 var template;
 //Load jQuery Mobile the page shows
 $(document).on('pagebeforeshow', function() {
 	template = $('#handlebars').html();
-	$('div').remove();
-});
+//	$('div').remove();
+});	
 
 $(document).on('pagecreate', function() {
 
 	//GetPosition and Get Weather ChainedV
 	getPosition().done(function () {
 		console.log('Execute getPositon.done() ');		
-		getWeather().done(function () {
+		getCurrentWeather().done(function () {
 			console.log('Execute getWeather.done()');
 			getTemplate();
 		});//getWeather
 	})//getPosition
+
+//Reference:http://stackoverflow.com/questions/14468659/jquery-mobile-document-ready-vs-page-events/14469041#14469041	
+	$(document).on('click', '#button', function(e) {
+		debugger;
+		console.log(userLocation);
+			getWeatherForecast().done(function() {
+				console.log('Execute getCurrentWeather.done()');	
+			});
+	});
 });
+
 
 	function getPosition() {
 		var deferred = $.Deferred();
 		navigator.geolocation.getCurrentPosition(function(position) {
-			//Pass data to see if it GETS the positions
+		
+//Pass data to see if it GETS the positions
 			console.log(position.coords.latitude + ' ' + position.coords.longitude);
 			userLocation.latitude = position.coords.latitude;
 			userLocation.longitude = position.coords.longitude;
@@ -33,27 +44,48 @@ $(document).on('pagecreate', function() {
 	}
 
 
-	function getWeather() {
+	function getCurrentWeather() {
 		var deferred = $.Deferred();
 		url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + userLocation.latitude + '&lon=' + userLocation.longitude + '&units=metric' + '&appid=a8e29972d260d0df1639bf0d7199f680';
 
-   		//Get weather info using http://openweathermap.org/current
 		$.getJSON(url, function(weatherJson) {
       			//Show data
-         		//prettyPrint = JSON.stringify(weatherJson, null, '\t');
-			//console.log(prettyPrint);
-			weatherInfo.weather = weatherJson;
+         		prettyPrint = JSON.stringify(weatherJson, null, '\t');
+			console.log(prettyPrint);
+			weatherInfo = weatherJson;
 			deferred.resolve();
 		});//getJSON
 		return deferred.promise();
 	}//getWeather
 	
+	function getWeatherForecast() {
+		var deferred = $.Deferred();
+		url = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + userLocation.latitude + '&lon=' + userLocation.longitude + '&units=metric&appid=a8e29972d260d0df1639bf0d7199f680&cnt=7' 
+
+		$.getJSON(url, function(weatherJson) {
+      			//Show data
+         		prettyPrint = JSON.stringify(weatherJson, null, '\t');
+			console.log(prettyPrint);
+			//weatherInfo = weatherJson;
+			deferred.resolve();
+		});//getJSON
+		return deferred.promise();
+	}//getWeather
+
 	function getTemplate() {
-			var templateScript = Handlebars.compile(template);
-			var html = templateScript(weatherInfo.weather.main);
-			$(document.body).append(html);
+		//console.log(template);	
+		var templateScript = Handlebars.compile(template);
+		//console.log(templateScript);
+		/*weatherInfo is global right now*/
+		var html = templateScript(weatherInfo);
+		//console.log(html);
+		$(document.body).append(html);
 	}
 
+//******Handlebar Helper Functions*****************//
+	Handlebars.registerHelper('computeIcon', function() {
+	  	return'https://openweathermap.org/img/w/' + weatherInfo.weather[0].icon + '.png';
+	});
 /*if (navigator.geolocation) {
   var latitude;
   var longitude;
@@ -150,6 +182,7 @@ function returnIcon(id) {
   var urlIcon = 'https://openweathermap.org/img/w/' + id + '.png';
   return urlIcon;
 }
+
 
 //Formula: 
 //Celsius to Fahrenheit:   (°C × 9/5) + 32 = °F
